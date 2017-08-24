13cc6eabf01b4b4c817c40501e96838574360661
Diego Camacho <hackvan@gmail.com>
2017-08-24 13:39:43 -0500

    Add validation on Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
+  validates :title, presence: { message: "debe ser ingresado." }
+  validates :body,  presence: { message: "debe ser ingresado." }
+  validates :body,  length: { minimum: 250,
+                              too_short: 'El cuerpo del post debe contener como minimo %{count} caracteres.' }
 end
