8aa8e37993abbcd259ba34881d2206d04ce90c90
Diego Camacho <hackvan@gmail.com>
2017-08-25 13:48:44 -0500

    Add I18N locales es.yml and devise.es.yml with custom attributes and messages errors

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,7 @@
 class Post < ApplicationRecord
-  validates :title, presence: { message: "debe ser ingresado." }
-  validates :body,  presence: { message: "debe ser ingresado." }
-  validates :body,  length: { minimum: 250,
-                              too_short: 'El cuerpo del post debe contener como minimo %{count} caracteres.' }
+  validates :title, presence: true
+  validates :body,  presence: true
+  validates :body,  length: { minimum: 250 }
   belongs_to :user
   validates_associated :user
   has_many :comments
