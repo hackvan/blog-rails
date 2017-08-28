68a060d66b3387bc2236f3c13bfbd6c09c6c19d3
Diego Camacho <hackvan@gmail.com>
2017-08-28 14:15:56 -0500

    Adding Bootstrap framework and custom visual styles

--- Gemfile
+++ Gemfile
@@ -35,6 +35,8 @@ gem 'jbuilder', '~> 2.5'
 
 gem 'devise', '~> 4.3'
 gem 'redcarpet', '~> 3.4'
+gem 'bootstrap-sass', '~> 3.3.6'
+gem 'jquery-rails', '~> 4.3', '>= 4.3.1'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,14 @@
 module ApplicationHelper
+  def flash_class(type)
+    case type
+      when "success" then "success"
+      when "error"   then "danger"
+      when "alert"   then "warning"
+      when "notice"  then "info"
+      else "info"
+    end
+  end
+
   def markdown(text)
     options = { hard_wrap: true,
                 filter_html: false,
@@ -12,4 +22,5 @@ module ApplicationHelper
     markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, options)
     return markdown.render(text).html_safe
   end
+
 end
